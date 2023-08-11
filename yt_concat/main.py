324604f73a85f713f3f87b808421f73be5214d13
Jim-git-99 <tw194102753huang@gmail.com>
2023-08-11 13:21:00 +0800

    Refactor code into pipeline design pattern

--- yt_concat/main.py
+++ yt_concat/main.py
@@ -1,36 +1,20 @@
-import urllib.request
-import json
-from settings import API_KEY
+from pipeline.steps.get_video_list import GetVideoList
+from pipeline.pipeline import Pipeline
 
 CHANNEL_ID = 'UC7o64rkXin-ZS_gU6_pBDvA'
-api_key = API_KEY
-print(api_key)
 
-def get_all_video_in_channel(channel_id):
-    
+def main():
+    inputs = {                    # Initial constant input
+        'channel_id' : CHANNEL_ID
+    }
 
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+    steps = [
+        GetVideoList(),
+    ]
 
-    first_url = base_search_url+'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key, channel_id)
-
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except:
-            break
-    return video_links
+    p = Pipeline(steps)
+    p.run(inputs)
 
 
-# video_url_list = get_all_video_in_channel(CHANNEL_ID)
-# print(len(video_url_list))
+if __name__ == '__main__':
+    main()
